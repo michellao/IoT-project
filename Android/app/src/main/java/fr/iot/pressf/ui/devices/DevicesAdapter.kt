843487de70f427a2042ee50db717e32a25d01090
Michel LAO <SansNom01@proton.me>
2023-12-20 14:56:51 +0100

    Preparation Recycler View

---
+++ Android/app/src/main/java/fr/iot/pressf/ui/devices/DevicesAdapter.kt
@@ -0,0 +1,31 @@
+package fr.iot.pressf.ui.devices
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fr.iot.pressf.R
+
+class DevicesAdapter(private val devices: Array<String>) :
+    RecyclerView.Adapter<DevicesAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView
+        init {
+            name = view.findViewById(R.id.device_text_view)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.device_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = devices.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name.text = devices[position]
+    }
+
+}
