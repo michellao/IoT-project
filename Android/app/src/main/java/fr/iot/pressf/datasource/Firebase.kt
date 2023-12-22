6f381e737ab610750cf562dd0112ff82a11ee124
Michel LAO <SansNom01@proton.me>
2023-12-22 12:00:37 +0100

    Update data class Data

--- Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
+++ Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
@@ -1,10 +1,11 @@
 package fr.iot.pressf.datasource
 
 import com.google.firebase.database.IgnoreExtraProperties
+import java.security.Timestamp
 
 @IgnoreExtraProperties
 data class Device(val name: String? = null)
 
 @IgnoreExtraProperties
-data class Data(val temperature: Float, val humidity: Float)
+data class Data(val temperature: Float? = null, val humidity: Float? = null, val timestamp: Timestamp? = null)
 
