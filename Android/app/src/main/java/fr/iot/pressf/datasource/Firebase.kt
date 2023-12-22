0cf60586858e70456c7f4b0eddd3f02d58e9e7d6
Michel LAO <SansNom01@proton.me>
2023-12-22 09:35:13 +0100

    Add new data class Data

--- Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
+++ Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
@@ -5,3 +5,6 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 data class Device(val name: String? = null)
 
+@IgnoreExtraProperties
+data class Data(val temperature: Float, val humidity: Float)
+
