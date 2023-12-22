0f8a674054dda06020bd32274d46bed655ace136
Lucas <lucas.lefebvre@etu.univ-lyon1.fr>
2023-12-22 12:27:02 +0100

    Add data

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -4,6 +4,11 @@
 #include "FeatherShieldESP32Pinouts.h"
 #include "Zanshin_BME680.h"
 
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
 // Provide the token generation process info.
 #include <addons/TokenHelper.h>
 
@@ -22,14 +27,14 @@
 #define DATABASE_URL "esp32-13749-default-rtdb.europe-west1.firebasedatabase.app" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /** Define the database secret (optional)
- *
- * This database secret needed only for this example to modify the database rules
- *
- * If you edit the database rules yourself, this is not required.
- */
+  *
+  * This database secret needed only for this example to modify the database rules
+  *
+  * If you edit the database rules yourself, this is not required.
+  */
 #define DATABASE_SECRET "DATABASE_SECRET"
 
-#define DEVICE_ID "1"
+#define DEVICE_ID "0"
 #define DEVICE_NAME "Home"
 
 /* Define the Firebase Data object */
@@ -41,8 +46,13 @@ FirebaseAuth auth;
 /* Define the FirebaseConfig data for config data */
 FirebaseConfig config;
 
+FirebaseData stream;
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+
 unsigned long dataMillis = 0;
 int count = 0;
+bool fanValue = false;
 
 BME680_Class BME680;
 
@@ -53,114 +63,203 @@ struct SensorReading {
   int32_t gas;
 };
 
+/* Initialization of the BME680 sensor (temperature, humidity, etc.) */
+void initBME680() {
+  Serial.print(F("- Initializing BME680 sensor\n"));
+  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
+    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
+    delay(5000);
+  }
+  Serial.print(F("- Setting 16x oversampling for all sensors\n"));
+  BME680.setOversampling(TemperatureSensor, Oversample16);
+  BME680.setOversampling(HumiditySensor, Oversample16);
+  BME680.setOversampling(PressureSensor, SensorOff);
+
+  Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
+  BME680.setIIRFilter(IIR4);
+
+  Serial.print(F("- Disable gas measurement\n"));
+  BME680.setGas(320, 0);
+}
+
+/* Wi-Fi initialization */
+void initWifi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+void streamCallback(StreamData data) {
+  Serial.printf("sream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
+                data.streamPath().c_str(),
+                data.dataPath().c_str(),
+                data.dataType().c_str(),
+                data.eventType().c_str());
+  printResult(data); // see addons/RTDBHelper.h
+  Serial.println();
+  bool value = data.to<bool>();
+  if(value){
+    analogWrite(A5, 200);
+    show("Etat du ventilteur : true");
+  }
+  else {
+      analogWrite(A5, 0);
+      show("Etat du ventilteur : false");
+  }
+
+  // This is the size of stream payload received (current and max value)
+  // Max payload size is the payload size under the stream path since the stream connected
+  // and read once and will not update until stream reconnection takes place.
+  // This max value will be zero as no payload received in case of ESP8266 which
+  // BearSSL reserved Rx buffer size is less than the actual stream payload.
+  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
+}
+
+void streamTimeoutCallback(bool timeout)
+{
+  if (timeout)
+    Serial.println("stream timed out, resuming...\n");
+
+  if (!stream.httpConnected())
+    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
+}
+
 void setup() {
 
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    Serial.print(F("- Initializing BME680 sensor\n"));
-    while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
-      Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
-      delay(5000);
-    }
+  initBME680();
+  initWifi();
 
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
-    }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
+  /**
+  * FIREBASE CONFIGURATION
+  */
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+  /* Assign the RTDB URL */
+  config.database_url = DATABASE_URL;
 
-    /* Assign the api key (required) */
-    config.api_key = API_KEY;
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
 
-    /* Assign the user sign in credentials */
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
 
-    /* Assign the RTDB URL */
-    config.database_url = DATABASE_URL;
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+  fbdo.setResponseSize(4096);
 
-    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
-    Firebase.reconnectNetwork(true);
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
 
-    // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
-    // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
-    fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+  /* Initialize the library with the Firebase authen and config */
+  Firebase.begin(&config, &auth);
 
-    fbdo.setResponseSize(4096);
+  stream.keepAlive(5, 5, 1);
+  if (!Firebase.beginStream(stream, "/devices/" + String(DEVICE_ID) + "/fan"))
+    Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
 
-    /* Assign the callback function for the long running token generation task */
-    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  Firebase.setStreamCallback(stream, streamCallback, streamTimeoutCallback);
+  /** Now modify the database rules (if not yet modified)
+    *
+    * The user, <user uid> in this case will be granted to read and write
+    * at the certain location i.e. "/UsersData/<user uid>".
+    *
+    * If you database rules has been modified, please comment this code out.
+    *
+    * The character $ is to make a wildcard variable (can be any name) represents any node key
+    * which located at some level in the rule structure and use as reference variable
+    * in .read, .write and .validate rules
+    *
+    * For this case $userId represents any <user uid> node that places under UsersData node i.e.
+    * /UsersData/<user uid> which <user uid> is user UID.
+    *
+    * Please check your the database rules to see the changes after run the below code.
+  */
+  String var = "$userId";
+  String val = "($userId === auth.uid && auth.token.premium_account === true && auth.token.admin === true)";
+  Firebase.setReadWriteRules(fbdo, "", var, val, val, DATABASE_SECRET);
 
-    /* Initialize the library with the Firebase authen and config */
-    Firebase.begin(&config, &auth);
+  Firebase.setString(fbdo, ("/devices/" + String(DEVICE_ID) + "/name").c_str(), DEVICE_NAME);
 
-    /** Now modify the database rules (if not yet modified)
-     *
-     * The user, <user uid> in this case will be granted to read and write
-     * at the certain location i.e. "/UsersData/<user uid>".
-     *
-     * If you database rules has been modified, please comment this code out.
-     *
-     * The character $ is to make a wildcard variable (can be any name) represents any node key
-     * which located at some level in the rule structure and use as reference variable
-     * in .read, .write and .validate rules
-     *
-     * For this case $userId represents any <user uid> node that places under UsersData node i.e.
-     * /UsersData/<user uid> which <user uid> is user UID.
-     *
-     * Please check your the database rules to see the changes after run the below code.
-     */
-    String var = "$userId";
-    String val = "($userId === auth.uid && auth.token.premium_account === true && auth.token.admin === true)";
-    Firebase.setReadWriteRules(fbdo, "", var, val, val, DATABASE_SECRET);
+  /*
+  if (!Firebase.beginStream(fbdo, "/devices/" + String(DEVICE_ID) + "/fan")) {
+    Serial.println("Erreur de démarrage du flux : " + fbdo.errorReason());
+  }
+  /**/
 
-    Firebase.setString(fbdo, ("/devices/" + String(DEVICE_ID) + "/name").c_str(), DEVICE_NAME);
-
-    pinMode(A0,OUTPUT);
-
-    Serial.print(F("- Setting 16x oversampling for all sensors\n"));
-    BME680.setOversampling(TemperatureSensor, Oversample16);
-    BME680.setOversampling(HumiditySensor, Oversample16);
-    BME680.setOversampling(PressureSensor, SensorOff);
-
-    Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
-    BME680.setIIRFilter(IIR4);
-
-    Serial.print(F("- Disable gas measurement\n"));
-    BME680.setGas(320, 0);
+  // Screen monitor configuration
+  display.begin(0x3C, true);
 }
 
 
 
 void loop() {
-  if (millis() - dataMillis > 1000 && Firebase.ready()) {
+/*
+  if (Firebase.getBool(fbdo, "/devices/" + String(DEVICE_ID) + "/fan") && Firebase.ready()) {
+
+    if (fbdo.boolData() == fanValue){
+      show("Etat du ventilteur OK");
+    }
+    else if (fbdo.boolData()){
+      fanValue = true;
+      show("Etat du ventilteur : true");
+    }
+    else {
+      fanValue = false;
+      show("Etat du ventilteur : false");
+    }
+    
+  }
+
+  if(fanValue) {
+      analogWrite(A5, 200);
+    }
+    else {
+      analogWrite(A5, 0);
+  }*/
+
+  if (millis() - dataMillis > 2000 && Firebase.ready()) {
       dataMillis = millis();
       
-      if (count >= 150){
+      if (count > 150){
           count = 0;
       }
-      // Firebase.ready() should be called repeatedly to handle authentication tasks.
     
       SensorReading sensorData;
       BME680.getSensorData(sensorData.temperature, sensorData.humidity, sensorData.pressure, sensorData.gas);  // Get readings
 
-      String path = "/data-1";
+      String path = "/data/" + String(DEVICE_ID);
 
+      Firebase.setIntAsync(fbdo, path+"/latest", count);
+
+      Serial.printf((String(count) + " | Device " + String(DEVICE_ID) + " - set temperature value : %s\n").c_str(), Firebase.setFloat(fbdo, path + "/" + count +"/temperature", sensorData.temperature / 100.0) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf((String(count) + " | Device " + String(DEVICE_ID) + " - set humidity value : %s\n").c_str(), Firebase.setFloat(fbdo, path + "/" + count +"/humidity", sensorData.humidity / 1000.0) ? "ok" : fbdo.errorReason().c_str());
+      Serial.printf((String(count) + " | Device " + String(DEVICE_ID) + " - set timestamp value : %s\n").c_str(), Firebase.setTimestamp(fbdo, path + "/" + count +"/timestamp") ? "ok" : fbdo.errorReason().c_str());
       count++;
+  }
+  else if (!Firebase.ready()) {
+    Serial.println("Firebas not ready !");
+  }
+}
 
-      Firebase.setInt(fbdo, "/latest", count);
-
-      Serial.printf((String(count) + " | Device 1 - set temperature value : %s\n").c_str(), Firebase.setFloat(fbdo, path + "-" + count +"/temperature", sensorData.temperature / 100.0) ? "ok" : fbdo.errorReason().c_str());
-      Serial.printf((String(count) + " | Device 1 - set humidity value : %s\n").c_str(), Firebase.setFloat(fbdo, path + "-" + count +"/humidity", sensorData.humidity / 1000.0) ? "ok" : fbdo.errorReason().c_str());
-
-    }
-    analogWrite(A5, 0);
+void show(String msg) {
+  display.clearDisplay();
+    display.setRotation(1);
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.setTextColor(SH110X_WHITE);
+    display.println(msg);
+    display.display();
 }
