fb83c1d65c623bb57ec8e2a38a8bdc4ab78ff746
Michel LAO <SansNom01@proton.me>
2024-01-15 12:22:46 +0100

    Get ViewModel by activity

--- Android/app/src/main/java/fr/iot/pressf/ui/devices/DevicesFragment.kt
+++ Android/app/src/main/java/fr/iot/pressf/ui/devices/DevicesFragment.kt
@@ -1,10 +1,12 @@
 package fr.iot.pressf.ui.devices
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import fr.iot.pressf.databinding.FragmentDevicesBinding
@@ -16,15 +18,13 @@ class DevicesFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val devicesViewModel: DevicesViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val devicesViewModel =
-            ViewModelProvider(this).get(DevicesViewModel::class.java)
-
         _binding = FragmentDevicesBinding.inflate(inflater, container, false)
         val root: View = binding.root
         devicesViewModel.fetchDevices()
