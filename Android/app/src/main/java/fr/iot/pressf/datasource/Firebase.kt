76ee6e55fccabe56a5cbedaeeeb6abe8f08a3285
Michel LAO <SansNom01@proton.me>
2024-01-15 11:59:47 +0100

    Update data

--- Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
+++ Android/app/src/main/java/fr/iot/pressf/datasource/Firebase.kt
@@ -4,7 +4,7 @@ import com.google.firebase.database.IgnoreExtraProperties
 import java.security.Timestamp
 
 @IgnoreExtraProperties
-data class Device(val name: String? = null)
+data class Device(val fan: Boolean? = null, val name: String? = null, val latest: Int? = null)
 
 @IgnoreExtraProperties
 data class Data(val temperature: Float? = null, val humidity: Float? = null, val timestamp: Long? = null)
