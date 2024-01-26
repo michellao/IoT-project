46a9dacc321225a14e86874af7d7940e7c00356d
Michel LAO <SansNom01@proton.me>
2024-01-26 17:31:03 +0100

    Display chart just fix timestamp

--- Android/app/src/main/java/fr/iot/pressf/ui/chart/ChartFragment.kt
+++ Android/app/src/main/java/fr/iot/pressf/ui/chart/ChartFragment.kt
@@ -1,47 +1,119 @@
 package fr.iot.pressf.ui.chart
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import fr.iot.pressf.databinding.FragmentChartBinding
+import fr.iot.pressf.ui.devices.DevicesViewModel
+import java.text.DateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ChartFragment : Fragment() {
+class ChartFragment : Fragment(),
+    OnSeekBarChangeListener,
+    OnChartValueSelectedListener {
 
     private var _binding: FragmentChartBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    var chartViewModel: ChartViewModel? = null
+    private lateinit var chartViewModel: ChartViewModel
+    private val devicesViewModel: DevicesViewModel by activityViewModels()
+
+    fun getCurrentCaptureTimestamp(): Long? {
+        val devices = devicesViewModel.devices.value
+        val device = devices?.get(0)
+        val index = device?.latest
+        val chartData = chartViewModel.data.value
+        Log.d("device", chartData.toString())
+        val data = index?.let { chartData?.get(it) }
+        val timestamp = data?.timestamp
+        return timestamp
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        chartViewModel =
-            ViewModelProvider(this).get(ChartViewModel::class.java)
+
         _binding = FragmentChartBinding.inflate(inflater, container, false)
+        chartViewModel = ViewModelProvider(this).get(ChartViewModel::class.java)
         val root: View = binding.root
+        val indexDevice = devicesViewModel.selected
+        chartViewModel.fetchData(indexDevice)
+        val entries: ArrayList<Entry> = ArrayList()
+        val chart = binding.chart
+        chartViewModel.data.observe(viewLifecycleOwner) {
+            val currentTimestamp = getCurrentCaptureTimestamp()
+            if (it != null && currentTimestamp != null) {
+                chart.setBackgroundColor(Color.WHITE)
+                chart.description.isEnabled = false
+                chart.setTouchEnabled(false)
+                entries.clear()
+                for ((index, item) in it.withIndex()) {
+                    if (item.temperature != null && item.humidity != null && item.timestamp != null) {
+                        val dateCapture = Date(item.timestamp)
+                        val dateString = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(dateCapture)
+                        entries.add(Entry(dateCapture.time.toFloat() - currentTimestamp, item.temperature))
+                    }
+                }
+            }
+            val dataSet = LineDataSet(entries, "Capteur")
+            Log.d("chart", dataSet.toString())
+            dataSet.color = Color.BLUE
+            val lineData = LineData(dataSet)
+            chart.data = lineData
+            chart.invalidate()
+        }
+
+        /*val seekBarX = binding.seekBarX
+        val seekBarY = binding.seekBarY
+
+        seekBarX.setOnSeekBarChangeListener(this)
+
+        seekBarY.max = 100
+        seekBarY.setOnSeekBarChangeListener(this)*/
 
         return root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val textView: TextView = binding.roomName
-        chartViewModel!!.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStartTrackingTouch(p0: SeekBar?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopTrackingTouch(p0: SeekBar?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onValueSelected(p0: Entry?, p1: Highlight?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected() {
+        TODO("Not yet implemented")
     }
 }
--- Android/app/src/main/java/fr/iot/pressf/ui/chart/ChartViewModel.kt
+++ Android/app/src/main/java/fr/iot/pressf/ui/chart/ChartViewModel.kt
@@ -3,24 +3,29 @@ package fr.iot.pressf.ui.chart
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.Firebase
 import com.google.firebase.database.*
 import fr.iot.pressf.datasource.Data
+import kotlinx.coroutines.launch
 
 class ChartViewModel : ViewModel() {
-    val data = MutableLiveData<List<Data>>()
+    val data = MutableLiveData<List<Data>?>()
 
-    fun fetchData() {
-        val dataRef = Firebase.database.getReference("/data/0")
-        dataRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val value = dataSnapshot.getValue<List<Data>>()
-                Log.d("database", "$value")
-            }
+    fun fetchData(idDevice: Int) {
+        viewModelScope.launch {
+            val dataRef = Firebase.database.getReference("/data/$idDevice")
+            dataRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val value = dataSnapshot.getValue<List<Data>>()
+                    data.postValue(value)
+                    Log.d("database", "$value")
+                }
 
-            override fun onCancelled(db: DatabaseError) {
-                Log.e("database", "Error while fetching data: ", db.toException())
-            }
-        })
+                override fun onCancelled(db: DatabaseError) {
+                    Log.e("database", "Error while fetching data: ", db.toException())
+                }
+            })
+        }
     }
 }
